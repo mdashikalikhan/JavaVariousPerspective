08430ea63291c91dcb34f6a6f24e3d8db3204822
ashik.khan <ashik.khan@N1084.spftl.com>
2018-04-30 16:07:15 +0600

    Finding the  distinct pairs of integers in a SET that sum up to an integer

---
+++ src/main/java/com/practice/Numbers/SummationPairs.java
@@ -0,0 +1,76 @@
+package com.practice.Numbers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+public class SummationPairs {
+
+	/**
+	 * 
+	 * @param args
+	 * 
+	 * SUM = N
+	 * 
+	 * SET1 = {n1,n2,n3,n4}
+	 * SET1 = {n5,n6,n7,n8}
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("SUMMATION= " );
+		Scanner summationInput = new Scanner(System.in);
+		int sum = summationInput.nextInt();
+		
+		System.out.println("Number of elements in SET");
+		
+		Scanner setSizeInput = new Scanner(System.in);
+		
+		while(setSizeInput.hasNextInt()) {
+			
+				int noOfElements =  setSizeInput.nextInt();
+				System.out.println("Give "+ noOfElements  +" Set Elements");
+				
+				int[] dataSetArray = new int [noOfElements];
+				
+				for(int i=0; i<noOfElements; i++) {
+					dataSetArray[i] = new Scanner(System.in).nextInt();
+				}
+				printPairs(dataSetArray,  sum);
+				System.out.println("Number of elements in SET");
+		}
+		
+	}
+	
+	public static void printPairs(int[] dataSetArray, int sum) {
+		
+		Map<Integer, Integer> mapNumbers = new HashMap<Integer, Integer>();
+		
+		for(int data : dataSetArray) {
+			if(mapNumbers.containsKey(sum-data) && mapNumbers.get(sum-data)==0) {
+					System.out.println((sum-data) + ", " + data);
+					mapNumbers.put((sum-data), mapNumbers.get(sum-data)+1);
+			} else if (!mapNumbers.containsKey(data)) {
+				mapNumbers.put(data, 0);
+			} 
+		}
+		
+	}
+	
+	public static int SumPairs(int[] input, int k) {
+
+		Map<Integer, Integer> pairs = new HashMap<Integer, Integer>();
+		int tmp = 0;
+		for (int data : input) {
+			if (pairs.containsKey(k - data) && pairs.get(k - data) == 0) {
+				tmp++;
+				pairs.put((k - data), pairs.get(k - data) + 1);
+			} else if (!pairs.containsKey(data)) {
+				pairs.put(data, 0);
+			}
+		}
+
+		return tmp;
+	}
+}
