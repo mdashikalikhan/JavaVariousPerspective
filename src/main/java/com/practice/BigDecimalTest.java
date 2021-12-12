ec22ed18676de4a70bb6e306eda9b527cb9c3907
ashik.khan <ashik.khan@N1084.sonaliintellect.com>
2021-12-12 15:26:49 +0600

    #Commit all

--- src/main/java/com/practice/BigDecimalTest.java
+++ src/main/java/com/practice/BigDecimalTest.java
@@ -1,10 +1,11 @@
 package com.practice;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BigDecimalTest {
 	public static void main(String[] args) {
-		BigDecimal amount = new BigDecimal( 12233124.533432);
+		/*BigDecimal amount = new BigDecimal( 12233124.533432);
 		BigDecimal factor = new BigDecimal(100000);
 		amount = amount.divide(factor);
 		System.out.println(amount);
@@ -17,6 +18,25 @@ public class BigDecimalTest {
 		System.out.println(val1.setScale(2, BigDecimal.ROUND_UP));
 		
 		System.out.println(val1.setScale(3, BigDecimal.ROUND_HALF_UP));
-		System.out.println(val1.setScale(2, BigDecimal.ROUND_HALF_UP));
+		System.out.println(val1.setScale(2, BigDecimal.ROUND_HALF_UP));*/
+		
+		/*bookVal=bookVal.add((arbitrarySelection.getBookValue().divide(arbitrarySelection.getAvailableQuantity())
+				.multiply(arbitrarySelection.getUtilizedQuantity())));*/
+		
+		/*BigDecimal sum = BigDecimal.ZERO;
+		BigDecimal bookVal = new BigDecimal("777455560.001");
+		
+		BigDecimal quantity = new BigDecimal("70000000.878");
+		BigDecimal utilized = new BigDecimal("500.01");
+		
+		sum = sum.add(bookVal).divide(quantity,8, RoundingMode.HALF_UP).multiply(utilized);
+		System.out.println(sum);*/
+		
+		BigDecimal val1 = new BigDecimal("6353545435435");
+		BigDecimal val2 = new BigDecimal("6353545435435");
+		
+		System.out.println(val1.setScale(0, RoundingMode.CEILING));
+		
+		
 	}
 }
