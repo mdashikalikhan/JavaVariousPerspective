e555147c0e1110e4276c97bd07f174400446c863
mdashikalikhan <khan.ashik@gmail.com>
2022-01-13 13:05:04 +0600

    Leetcode Reverse Integer problem 7 solved.

---
+++ src/main/java/com/leetcode/practice/ReverseInteger.java
@@ -0,0 +1,50 @@
+package com.leetcode.practice;
+
+public class ReverseInteger {
+	public static void main(String[] args) {
+		int num1 =  -2147483648;
+		System.out.println(reverse(num1));
+	}
+	
+	public static int reverse(int x) {
+
+		long num = x;
+		
+		if(x<0) {
+			num = -num;
+		}
+		
+		if(num>Integer.MAX_VALUE) {
+			return 0;
+		}
+		
+		
+		long rev=0;
+		while(num!=0) {
+			int rem = (int)num%10;
+			rev = rev * 10 + rem;
+			num = num/10;
+		}
+		
+		if(rev>Integer.MAX_VALUE) {
+			return 0;
+		}
+		return (int)(x<0?-rev:rev);
+	}
+	
+	/**
+	 * class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+	 */
+}
