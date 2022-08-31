12a7e31e5016608e0c43eec7dd609299d2e8c09a
mdashikalikhan <khan.ashik@gmail.com>
2022-08-31 16:35:47 +0600

    static and non-static overriding test

---
+++ src/main/java/com/interview/Base1.java
@@ -0,0 +1,11 @@
+package com.interview;
+
+public class Base1 {
+    public static int getArea(int a, int b){
+        return a * b;
+    }
+
+    public int getLength(int a, int b){
+        return a + b;
+    }
+}
--- src/main/java/com/interview/Polymorphism.java
+++ src/main/java/com/interview/Polymorphism.java
@@ -2,6 +2,8 @@ package com.interview;
 
 public class Polymorphism {
     public static void main(String[] args) {
-        
+        Base1 base1 = new Child1();
+        System.out.println(base1.getArea(10, 20));
+        System.out.println(base1.getLength(10, 20));
     }
 }
